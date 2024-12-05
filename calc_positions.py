5073387fcaa2f37a01a0551a8e0b7aebc6a3874d
MaxPett <124563194+MaxPett@users.noreply.github.com>
2024-12-05 20:27:36 +0100

    update coordinate calculation

--- calc_positions.py
+++ calc_positions.py
@@ -2,23 +2,26 @@ import numpy as np
 import pandas
 import pandas as pd
 
-HOME_COORDINATES_X = 438.75
-HOME_COORDINATES_Y = -77.3
-HOME_COORDINATES_Z = 10.15
+HOME_COORDINATES_X = 430
+HOME_COORDINATES_Y = -280
+HOME_COORDINATES_Z = 400
 HOME_ROTATION_RX = 0.420
 HOME_ROTATION_RY = -2.375
 HOME_ROTATION_RZ = 2.381
 
-START_COORDINATES_X = 395.0
-START_COORDINATES_Y = -115
-START_COORDINATES_Z = 17
-START_ROTATION_RX = 0.627
-START_ROTATION_RY = -1.766
-START_ROTATION_RZ = 1.678
+START_COORDINATES_X = 300
+START_COORDINATES_Y = -200
+START_COORDINATES_Z = 300
+START_ROTATION_RX = 0.487
+START_ROTATION_RY = -1.767
+START_ROTATION_RZ = 1.580
 
-CUBE_SIDE_LENGTH = 50
+CUBE_SIDE_LENGTH = 20
 ROTATION_STEP_ANGLE = 5  # in degrees
 ROTATION_STEPS = 2
+
+UR3E_MIN_OPERATING = 200
+UR3E_MAX_OPERATING = 500
 
 
 def point_rotation(curr_coord, rot_angle, nr_steps):
@@ -65,8 +68,8 @@ def create_horizontal_in_plane_points(df, delta_length):
 
 
 def generate_coordinates_matrix(home_point, start_point, side_length, step_angle, steps):
-    settings_df = pd.DataFrame([start_point - home_point])
-
+    settings_df = pd.DataFrame([home_point])
+    settings_df = pd.concat([settings_df, pd.DataFrame([start_point])], ignore_index=True)
     change_length = side_length / 2
     current_point = settings_df.iloc[-1].copy()
 
@@ -90,37 +93,45 @@ def generate_coordinates_matrix(home_point, start_point, side_length, step_angle
     settings_df = pd.concat([settings_df, pd.DataFrame([current_point])], ignore_index=True)
     settings_df = create_horizontal_in_plane_points(settings_df, change_length)
 
-    # Return to home
-    vector_to_home_point = pd.Series(home_point) - settings_df.iloc[-1]
-    settings_df = pd.concat([settings_df, pd.DataFrame([vector_to_home_point])], ignore_index=True)
-
     # Add rotation to each point, except the start point
     coordinates_df = pd.DataFrame()
     for i in range(len(settings_df)):
-        if i in [0, len(settings_df)-1]:
+        if i in [0, 1]:
             coordinates_df = pd.concat([coordinates_df, pd.DataFrame(settings_df.iloc[[i]])], ignore_index=True)
         else:
             coordinates_df = pd.concat([coordinates_df, settings_df.iloc[[i]]], ignore_index=True)
             rotated_point = point_rotation(settings_df.iloc[[i]], step_angle, steps)
             coordinates_df = pd.concat([coordinates_df, rotated_point], ignore_index=True)
+    coordinates_df = pd.concat([coordinates_df, pd.DataFrame([home_point])], ignore_index=True)
     coordinates_df.columns = ['X', 'Y', 'Z', 'RX', 'RY', 'RZ']
-    metric_coords = coordinates_df.copy()
-    metric_coords.iloc[:, :3] = metric_coords.iloc[:, :3] * 0.001
-    metric_coords.to_csv("ur3e_coordinates.csv")
     return coordinates_df
 
 
-def calculate_change_vectors(df, np_home):
+def calculate_change_vectors(df):
     result_df = pd.DataFrame(columns=df.columns)
-
     # Iterate through the DataFrame and perform the subtraction
-    for i in range(len(df) - 2):
+    for i in range(len(df)-1):
         result_df = pd.concat([result_df, pd.DataFrame([df.iloc[i + 1] - df.iloc[i]])], ignore_index=True)
-
-    # Handle the last row by subtracting the first row
-    last_row_result = np_home - df.iloc[-2]
-    result_df = pd.concat([result_df, pd.DataFrame([last_row_result])], ignore_index=True)
     return result_df
+
+
+def check_boundaries(ur3e_min, ur3e_max, df):
+    df_check = df[1:-1]
+    res_vec_square = df_check['X']*df_check['X']+df_check['Y']*df_check['Y']+df_check['Z']*df_check['Z']
+    max_magnitude = np.sqrt(np.max(res_vec_square))
+    min_magnitude = np.sqrt(np.min(res_vec_square))
+    if min_magnitude < ur3e_min:
+        delta = ur3e_min - min_magnitude
+        raise ValueError(f"Safety distance to the base of the robot is too small by {delta} mm.")
+
+    if max_magnitude > ur3e_max:
+        delta = max_magnitude - ur3e_max
+        raise ValueError(f"Maximum travel distance of robot exceeded by {delta} mm.")
+
+    metric_coords = df.copy()
+    metric_coords.iloc[:, :3] = metric_coords.iloc[:, :3] * 0.001
+    metric_coords.to_csv("ur3e_coordinates.csv")
+    print("Coordinates calculated and values stored in ur3e_coordinates.csv!")
 
 
 if __name__ == '__main__':
@@ -129,5 +140,8 @@ if __name__ == '__main__':
     start = np.array([START_COORDINATES_X, START_COORDINATES_Y, START_COORDINATES_Z,
                       START_ROTATION_RX, START_ROTATION_RY, START_ROTATION_RZ])
     df_coord = generate_coordinates_matrix(home, start, CUBE_SIDE_LENGTH, ROTATION_STEP_ANGLE, ROTATION_STEPS)
-    df_change = calculate_change_vectors(df_coord, home)
-    print(df_coord)
+    # Change vectors not required for ur3e control
+    df_change = calculate_change_vectors(df_coord)
+
+    # Check if all points can be reached
+    check_boundaries(UR3E_MIN_OPERATING, UR3E_MAX_OPERATING, df_coord)
